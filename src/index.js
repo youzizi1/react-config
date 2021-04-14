f21639171221024e82b20a696161fa328fe058db
youzizi1 <yinyun957@gmail.com>
2021-04-14 18:01:10 +0800

    集成webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDom from 'react-dom'
 import App from './app'
 
 ReactDom.render(
-  <div>hello world
+  <div>hello world ugu
     <App />
   </div>,
   document.getElementById('app')
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require('path')
 
 module.exports = {
+  mode: 'development',
   entry: path.join(__dirname, 'src/index.js'),
   output: {
     path: path.join(__dirname, 'dist/'),
     filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist')
   },
   module: {
     rules: [
